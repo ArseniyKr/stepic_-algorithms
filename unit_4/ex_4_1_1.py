83f81b5b21920c1699e8e8e79801b2b9e65d8b32
Arseniy <Krupenin.A.G@sberbank.ru>
2018-05-22 08:57:49 +0300

    4.1.1

--- unit_4/ex_4_1_1.py
+++ unit_4/ex_4_1_1.py
@@ -1,4 +1,28 @@
-count = int(input())
+def sortByLastDot(inputSet):
+    return list(inputSet)[-1]
 
-for i in range(0, count):
-    n, m = map(int, input().split())
+countOfLines = int(input())
+setList = []
+for i in range(0, countOfLines):
+    x1, x2 = map(int, input().split())
+    linesSet = set(range(x1, x2 + 1))
+    setList.append(linesSet)
+
+setList.sort(key=sortByLastDot)
+
+resultSet = set()
+
+for setIdx in range(0,  len(setList) - 1):
+    resultSet = resultSet | (setList[setIdx] & setList[setIdx + 1])
+
+
+resultList = list(resultSet)
+resultList.sort()
+
+print(len(resultList))
+
+resultStr = ''
+for dot in resultSet:
+    resultStr += str(dot) + ' '
+
+print(resultStr)
